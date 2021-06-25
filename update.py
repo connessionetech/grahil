bca4e1d7268649055d1e0816706df6f9af8828ac
CTS <connessionetechnologies@gmail.com>
2021-06-26 01:28:25 +0530

    Updater update
    + Updated logic for building update package and overwriting original program
    + Added logic to verify update using error log
    
    + Updated log location and filename  for grahil

--- update.py
+++ update.py
@@ -8,6 +8,16 @@ import imp
 from jsonmerge import merge
 from jsonschema import validate
 import json
+import sys
+import time
+from pathlib import Path
+
+import logging
+
+
+# Configure the logging system
+logging.basicConfig(filename ='grahil_update.log',
+                    level = logging.ERROR)
 
 
 ''' Check if file is downloadable '''
@@ -29,7 +39,11 @@ def is_downloadable(url):
 # Download file
 url = 'https://grahil.s3.amazonaws.com/grahil-py.zip'
 current_program_path = "/home/rajdeeprath/github/grahil-py/"
+program_backup_path = "/home/rajdeeprath/grahil_backups/"
 versions_module_name = "version.py"
+backup_filename = "grahil_last_working"
+backup_format = "zip"
+error_log_file = "log/errors.log"
 
 def_conf_schema = {
             "properties" : {
@@ -115,17 +129,26 @@ temp_dir_for_updated = tempfile.TemporaryDirectory() # Where to build & test the
 
 if is_downloadable(url):
     # download file
+    logging.info("Downloading update from %s", url)
     path_to_zip_file = os.path.join(temp_dir_for_download.name, "grahil-latest.zip")
     r = requests.get(url, allow_redirects=True)
     open(path_to_zip_file, 'wb').write(r.content)
+
+    if os.path.exists(path_to_zip_file) and os.path.isfile(path_to_zip_file):
+        logging.info("Update downloaded to %s", path_to_zip_file)
+    else:
+        logging.error("Update download failed")
+        sys.exit()
     
     # extract file to a tmp location
     with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
         zip_ref.extractall(temp_dir_for_latest.name)
     path = os.path.join(temp_dir_for_latest.name, "run.py")
     if pathlib.Path(str(path)).exists():
-        print ("latest extraction success")
-        pass
+        logging.info("latest extraction success")
+    else:
+        logging.error("Failed to extract update content")
+        sys.exit()
     
     # copy existing program installation to a tmp location
     if os.path.exists(temp_dir_for_existing.name):
@@ -134,8 +157,10 @@ if is_downloadable(url):
     shutil.copytree(current_program_path, temp_dir_for_existing.name)
     path2 = os.path.join(temp_dir_for_existing.name, "run.py")
     if pathlib.Path(str(path2)).exists():
-        print ("existing installation copy success")
-        pass
+        logging.info("existing installation copy success")
+    else:
+        logging.error("Failed to copy existing program to tmp location")
+        sys.exit()
 
     ## compare versions
     old_version_module_path = os.path.join(temp_dir_for_existing.name, "oneadmin", "version.py")
@@ -154,26 +179,12 @@ if is_downloadable(url):
     elif (new_version[0] == old_version[0]) and  (new_version[1] == old_version[1]) and (new_version[2] > old_version[2]):
         upgrade=True
 
-    print("upgrade = "+str(upgrade))
+    logging.debug("upgrade %s", +str(upgrade))
     upgrade = True
 
 
     if upgrade:
-        print("ready to upgrade")
-
-
-
-
-        ## Merge configuration json files
-        #exisitng_configs_location=os.path.join(temp_dir_for_existing.name, "oneadmin", "modules", "config")
-        #new_configs_location=os.path.join(temp_dir_for_latest.name, "oneadmin", "modules", "config")
-
-        ## Merge reaction engine rules json files
-        #exisitng_rules_location=os.path.join(temp_dir_for_existing.name, "oneadmin", "modules", "config")
-        #new_rules_location=os.path.join(temp_dir_for_latest.name, "oneadmin", "modules", "config")
-
-
-
+        logging.info("Conditiosn valid to start upgrade")
 
         ## First we copy all old files into update workspace
         if os.path.exists(temp_dir_for_updated.name):
@@ -182,10 +193,14 @@ if is_downloadable(url):
         shutil.copytree(temp_dir_for_existing.name, temp_dir_for_updated.name)
         path2 = os.path.join(temp_dir_for_updated.name, "run.py")
         if pathlib.Path(str(path2)).exists():
-            print ("existing installation copy to update workspace success")
+            logging.info("existing installation copy to update workspace success")
+        else:
+            logging.error("Failed to copy existing program to update workspace")
+            sys.exit()
 
 
         # Collect list of all new files (json and otherwise)
+        logging.debug("Collecting list of all json files to be processed")
         latest_files = []
         latest_json_files = []
         for subdir, dirs, files in os.walk(temp_dir_for_latest.name):
@@ -207,6 +222,7 @@ if is_downloadable(url):
         ## check, validate and merge json configuration files        
         for file in latest_json_files:
             old_file_in_update_workspace = str(file).replace(temp_dir_for_latest.name, temp_dir_for_updated.name)
+            logging.debug("updating file %s", old_file_in_update_workspace)
 
             with open(old_file_in_update_workspace, 'r') as old_json_file:
                 base_data = old_json_file.read()
@@ -219,46 +235,92 @@ if is_downloadable(url):
                 if "conf/" in old_file_in_update_workspace:
                     validate(base_obj, def_conf_schema)
                     validate(latest_obj, def_conf_schema)
-                    updated_data = merge(base_obj, latest_obj)
+                    updated_data = merge(latest_obj, base_obj)
                 elif "rules/" in old_file_in_update_workspace:
                     validate(base_obj, def_rules_schema)
                     validate(latest_obj, def_rules_schema)
-                    updated_data = merge(base_obj, latest_obj)
+                    updated_data = merge(latest_obj, base_obj)
                 elif "configuration.json" in old_file_in_update_workspace:
                     validate(base_obj, def_master_configuration_schema)
                     validate(latest_obj, def_master_configuration_schema)
-                    updated_data = merge(base_obj, latest_obj)                    
+                    updated_data = merge(latest_obj, base_obj)
                 else:
-                    print("Unsure of how to merge thasi file... skipping")
+                    logging.warn("Unsure of how to update this file... skipping")
                     continue
-
 
                 with open(old_file_in_update_workspace, "w") as outfile:
                         outfile.write(json.dumps(updated_data))
-                    
-                    
 
-
-
-
+        
         ## verify everything
 
+        
         ## stop running program service
+        os.system("systemctl stop grahil.service")
+
 
         ## backup everything to a safe location
+        Path(program_backup_path).mkdir(parents=True, exist_ok=True)        
+        shutil.make_archive(backup_filename, backup_format, root_dir=program_backup_path, base_dir=current_program_path)
+        backup_file = os.path.join(program_backup_path, backup_filename + "." + backup_format)
+        if os.path.exists(backup_file):
+            logging.info("backup created @ %s", backup_file)
+        else:
+            logging.error("Failed to create backup of existing program")
+            sys.exit()
 
+        
         ## overwrite everything from prepared package to program location
+        for subdir, dirs, files in os.walk(temp_dir_for_updated.name):
+            for file in files:
+                    source = os.path.join(subdir, str(file))
+                    desitination = source.replace(temp_dir_for_updated.name, current_program_path)
+                    shutil.copy2(source, desitination)
+        
+        # Wait 5 seconmds
+        time.sleep(5)
 
-        ## start program
+        
+        ## Start service
+        os.system("systemctl restart grahil.service")
 
-        ## verify thisng are running ok and there are no errors
+
+        # Wait for service to start
+        time.sleep(15)
+
+
+        ## verify things are running ok and there are no errors from logs
+        TESTED_OK = False    
+
+        error_log = os.path.join(current_program_path, error_log_file)
+        if os.path.exists(error_log) and os.path.isfile(error_log):
+            file_size = os.path.getsize(error_log)
+            if file_size < 20:
+                TESTED_OK = True
+
 
         ## if anything is not working as expected cancel upgrade and restore backup
+        if not TESTED_OK:
+            logging.error("Upgrade failed. Reverting")
+            
+            ## stop service
+            os.system("systemctl stop grahil.service")
+            
+            time.sleep(5)
 
-        ## start program
+            for subdir, dirs, files in os.walk(temp_dir_for_existing.name):
+                for file in files:
+                        source = os.path.join(subdir, str(file))
+                        desitination = source.replace(temp_dir_for_existing.name, current_program_path)
+                        shutil.copy2(source, desitination)
+
+            time.sleep(5)
+
+            ## start service
+            os.system("systemctl restart grahil.service")
 
     else:
-        print("cannot upgrade")
+        logging.error("cannot upgrade")
 
 else:
-    print ("File is not downloadable")
+    logging.error("File is not downloadable")
