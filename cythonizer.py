43cd5b3bdc2b5678582dde0c9030a0cd375b6b0c
CTS <connessionetechnologies@gmail.com>
2021-06-29 19:20:46 +0530

    Cythonizer update
    + Updated cythonizer file

--- cythonizer.py
+++ cythonizer.py
@@ -1,7 +1,12 @@
 from setuptools import Extension, setup
 from Cython.Build import cythonize
 
-ext_modules = cythonize("oneadmin/modules/*.py")
+ext_modules = cythonize("oneadmin/modules/*.py",
+                        "communications.py", 
+                        "application.py",
+                        "configurations.py",
+                        "responsebuilder.py",
+                        "oneadmin/handlers/*.py",
+                        "oneadmin/security/*.py")
  
-setup(name='Hello world app',
-      ext_modules=cythonize(ext_modules))
+setup(name='Grahil', ext_modules=cythonize(ext_modules))
